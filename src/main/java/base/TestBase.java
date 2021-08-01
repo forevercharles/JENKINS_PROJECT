cc1b7807883a774d0355b8a7722caab36d663291
mdislam <mdislam@live.com>
2021-08-01 10:08:00 -0400

    Chromedriver updated

--- src/main/java/base/TestBase.java
+++ src/main/java/base/TestBase.java
@@ -22,8 +22,8 @@ public class TestBase {
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
-		driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get("https://techfios.com/billing/?ng=admin/");
 	}
 	
